fada16e3dbd7971b9fa10723e27bc4cf1285f715
B A Y R O N <bayron_sanchez@unitec.edu>
2024-10-18 22:45:05 -0600

    ejercicio de abstraccion

---
+++ Abstraccion/Circulo.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Abstraccion
+{
+    public class Circulo : Figura
+    {
+        public double Radio {  get; set; }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public override double Perimetro()
+        {
+            return 2*Math.PI * Radio;
+        }
+    }
+}
---
+++ Abstraccion/Figura.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Abstraccion
+{
+    public abstract class Figura{
+        public abstract double Area();
+        public abstract double Perimetro();
+        public void Imprimir(){
+            Console.WriteLine($"Area: {Area()}");
+            Console.WriteLine($"Perimetro: {Perimetro()}");
+        }
+    }
+}
